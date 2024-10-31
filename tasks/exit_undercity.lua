721d5b3659f52ca0d6c1999c7e3fa40205e6fe7c
TesXter <138005668+TesXter@users.noreply.github.com>
2024-10-31 18:05:32 +0700

    improve exit

--- tasks/exit_undercity.lua
+++ tasks/exit_undercity.lua
@@ -1,9 +1,11 @@
 local tracker = require "core.tracker"
+local utils = require "core.utils"
+local enums = require "data.enums"
 
 local task = {
     name = "Exit Undercity",
     shouldExecute = function()
-        return tracker.exit_undercity
+        return tracker.exit_undercity and tracker.player_in_boss_room and utils.player_in_find_zone(enums.zone_names.undercity_zone)
     end,
     Execute = function()
         tracker.exit_undercity = false
